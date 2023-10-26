295bceac18968166596f63a53a5bf38bc859b8c7
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-26 15:15:28 +0200

    Reticulating splines...

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
 
-  return (
+const [isFavorite, setIsFavorite] = useState(true)
+   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
+       onClick={() => {
+        setIsFavorite(!isFavorite);
+        // console.log("favorite button clicked");
       }}
       aria-label="favorite"
     >
